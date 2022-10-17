376e4cf687bfe0ace2650baf635afbe3c8b318c5
Alex Guillu <alex@Alexs-MBP.oswgilaa.metronetinc.net>
2022-10-17 09:41:52 -0500

    completed sprint challenge 13

--- index.js
+++ index.js
@@ -12,3 +12,14 @@ I need this code, but don't know where, perhaps should make some middleware, don
 
 Pull your server into this file and start it!
 */
+
+const dotenv = require('dotenv').config()
+
+const PORT = process.env.PORT || 9000
+
+
+const server = require('./api/server')
+
+server.listen( PORT, () => {
+    console.log('listening on', PORT)
+});
